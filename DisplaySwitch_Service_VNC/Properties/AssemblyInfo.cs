99a2e3dc5e69a107cc5c5507310cf0de97c5951f
Jack Naisbett <jack.naisbett@gmail.com>
2023-11-24 13:27:17 +0000

    Bump version

--- DisplaySwitch_Service_VNC/Properties/AssemblyInfo.cs
+++ DisplaySwitch_Service_VNC/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DisplaySwitch for RealVNC Server")]
-[assembly: AssemblyCopyright("Copyright © 2021")]
+[assembly: AssemblyCopyright("Copyright © 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("6.0.5.0")]
-[assembly: AssemblyFileVersion("6.0.5.0")]
+[assembly: AssemblyVersion("6.0.6.0")]
+[assembly: AssemblyFileVersion("6.0.6.0")]
 [assembly: NeutralResourcesLanguage("en-GB")]
 
